e1197183b2c81302078a69639b67aca84f710caa
bwesterg <benton.westergaard@gmail.com>
2021-08-07 20:14:04 +0200

    edit button now shows location data in a form.  Still need to add a close button and make the data changes persist.

--- src/components/LocationItem.js
+++ src/components/LocationItem.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import LocationForm from './LocationForm';
 
 export default function LocationItem({id, name, baseball, basketball, football, hockey, total_teams, capital, deleteLocation}){
 
+    const [isToggled, setIsToggled] = useState(false)
     const handleClick = (event) => deleteLocation(id)
-
-    return (
+    const handleToggle = (event) => setIsToggled(!isToggled)
+    const locationCard = () => (
         <li className="location-item">
             <h2>Location: {name}</h2>
             <h4>MLB team(s): {baseball}</h4>
@@ -14,7 +16,9 @@ export default function LocationItem({id, name, baseball, basketball, football, 
             <h4>Total professional teams: {total_teams}</h4>
             <h4>Any teams use state capital in their title? {capital}</h4>
             <button onClick={handleClick} className="delete-button">DELETE LOCATION</button>
-            <button className="edit-button">EDIT LOCATION</button>
+            <button onClick={handleToggle} className="edit-button">EDIT LOCATION</button>
         </li>
     )
+
+    return isToggled ? <LocationForm /> : locationCard()
 }
